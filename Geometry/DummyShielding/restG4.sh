2957962b45bf35fb4bdbcbb1a843ef23e617e110
girardcarillo <cloe.carillo@gmail.com>
2023-11-08 19:47:38 +0100

    working on common dummy geometry

---
+++ Geometry/DummyShielding/restG4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+restG4 Sim_ID_OV_Water200mm_Iron_Concrete.rml -t 12
+restG4 Sim_ID_OV_Water400mm_Iron_Concrete.rml -t 12
+
+restG4 Sim_ID_OV_Water200mm_Concrete_Iron.rml -t 12
+restG4 Sim_ID_OV_Water400mm_Concrete_Iron.rml -t 12
